b48694c53df8553ceb576ce7c5e2da090567f6e8
AbhishekSharma0008 <abhishekbhardwaj0008@gmail.com>
2024-11-05 10:14:47 +0530

    async await to fetch github

---
+++ day1/Day8/p1.js
@@ -0,0 +1,20 @@
+async function orderfood(name,time){
+    return new Promise((resolve,reject)=> {
+        setTimeout(()=>{
+            resolve("order for "+ name +" has been prepared");
+        },time);
+    });
+}
+async function restaurant(){
+    console.log("order food ");
+    let pizza=await orderfood("food ",2000);
+    console.log(pizza);
+    let burger=await orderfood("burger",3000);
+    console.log(burger);
+    let noodles=await orderfood("noodles",4000);
+    console.log(noodles);
+    console.log("ALL order prepared");
+    
+    
+}
+restaurant();
